47fdeeb8c85df91ff856749f7480995c5a87541f
NicolasMelet <nicolas.melet@epitech.eu>
2023-02-11 10:45:53 +0100

    test

---
+++ Assets/castle.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castle : MonoBehaviour
+{
+    public int life = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
