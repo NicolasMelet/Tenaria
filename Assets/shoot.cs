6c1b734593260d56f2c1c5b09105543bb9fb0c59
NicolasMelet <nicolas.melet@epitech.eu>
2023-02-12 11:19:03 +0100

    yes

---
+++ Assets/shoot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public player player;
+    public GameObject DarkProj;
+    public GameObject LightProj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            if (player.isDarkness)
+            {
+                GameObject projo = Instantiate(DarkProj, transform.position, Quaternion.identity) as GameObject;
+            }
+            else
+            {
+                GameObject projo = Instantiate(LightProj, transform.position, Quaternion.identity) as GameObject;
+            }
+        }
+    }
+}
