efb4a98d75f9475ea3cc33acb8b5c1dc0231f0e6
NicolasMelet <nicolas.melet@epitech.eu>
2023-02-12 21:33:28 +0100

    add killable bat

--- Assets/scripts/EnemyDeath.cs
+++ Assets/scripts/EnemyDeath.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class EnemyDeath : MonoBehaviour
 {
     public Health health;
-    public Rounds currentRound;
 
     // Update is called once per frame
     void Update()
     {
         if (health.GetHealth() <= 0)
         {
-            currentRound.enemysLeft -= 1;
             Destroy(gameObject);
         }
     }
